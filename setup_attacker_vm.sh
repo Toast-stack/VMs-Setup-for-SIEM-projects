845bde1aecb35f40908f553a12d066f7184645f6
Zachary Nicholas <115509575+Toast-stack@users.noreply.github.com>
2025-04-07 15:23:39 -0400

    Create setup_attacker_vm.sh

---
+++ setup_attacker_vm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#!/bin/bash
+echo "Starting Attacker Machine Configuration"
+
+# Update and install neccessary tools
+echo "Updating packages and installing Hydra and Nmap"
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y hydra nmap
+
+# Display the network configuration
+echo "Your Attacker Machine IP Address is:"
+ip addr show | grep "inet " | awk '{print $2}'
+
+echo "Attacker Machine Setup Complete! You can now use Hydra and Nmap to simulate attacks."
